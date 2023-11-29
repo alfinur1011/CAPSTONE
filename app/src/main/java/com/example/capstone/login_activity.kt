137873f06ed6d6cd33de19d4cde46c3b56b3d90a
alfinur063 <nurinayati1011@gmail.com>
2023-11-29 19:29:56 +0700

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/com/example/capstone/login_activity.kt
@@ -0,0 +1,13 @@
+package com.example.capstone
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class login_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
